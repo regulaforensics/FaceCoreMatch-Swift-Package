0909453af4e9e03dbda2ba9a9e1c1d6a386dd38d
Jenkins <jenkins@regula.by>
2026-04-22 12:13:37 +0300

    Release 8.2.2128

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["FaceCoreMatch"]),
     ],
     targets: [
-        .binaryTarget(name: "FaceCoreMatch", url: "https://pods.regulaforensics.com/FaceCoreMatch/8.1.1928/FaceCoreMatch-8.1.1928.zip", checksum: "642b783a223d2e2692b55ce9c8c8c7ed8e2376b0fece0bcf6bc5bf59fb651c35"),
+        .binaryTarget(name: "FaceCoreMatch", url: "https://pods.regulaforensics.com/FaceCoreMatch/8.2.2128/FaceCoreMatch-8.2.2128.zip", checksum: "fd4c666abe4bd45221bf44e3a7d540f7a9c19c33c808fc4db7e08ffe3e1f3789"),
     ]
 )
