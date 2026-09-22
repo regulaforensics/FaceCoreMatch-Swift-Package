b5fe7576b2d8d06202e01101ea1f8280c88826f0
Jenkins <jenkins@regula.by>
2026-09-22 15:43:13 +0300

    RC 8.4.2692

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["FaceCoreMatchStage"]),
     ],
     targets: [
-        .binaryTarget(name: "FaceCoreMatchStage", url: "https://pods.regulaforensics.com/Stage/FaceCoreMatchStage/8.4.2684/FaceCoreMatchStage-8.4.2684.zip", checksum: "dd8da8ee6de1e424d9ffe3480b3b6fa913125de5364d755e04f2e2cfb770dacc"),
+        .binaryTarget(name: "FaceCoreMatchStage", url: "https://pods.regulaforensics.com/Stage/FaceCoreMatchStage/8.4.2692/FaceCoreMatchStage-8.4.2692.zip", checksum: "7f19b6f78094d7f9c34640f2b95208f0e4af04bc8ebfaed1d411152ac8d1b9e2"),
     ]
 )
