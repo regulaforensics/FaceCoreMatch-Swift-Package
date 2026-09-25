09922aabe74b0f25b5e0b3ac241f51a431115923
Jenkins <jenkins@regula.by>
2026-09-25 15:52:43 +0300

    RC 8.4.2709

--- Package.swift
+++ Package.swift
@@ -1,15 +1,18 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "FaceCoreMatch",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "FaceCoreMatch",
             targets: ["FaceCoreMatchStage"]),
     ],
     targets: [
-        .binaryTarget(name: "FaceCoreMatchStage", url: "https://pods.regulaforensics.com/Stage/FaceCoreMatchStage/8.4.2707/FaceCoreMatchStage-8.4.2707.zip", checksum: "8f49a47b15eea047360a5b00354a68a81a5ae8b1a4efbe6d790f642fc9c3d1b8"),
+        .binaryTarget(
+            name: "FaceCoreMatchStage",
+            url: "https://pods.regulaforensics.com/Stage/FaceCoreMatchStage/8.4.2709/FaceCoreMatchStage-8.4.2709.zip",
+            checksum: "fe3fa2f7399ece114a745e82e731b3ae3feb24c6d2546bcc492f23a4911c395b"),
     ]
 )
